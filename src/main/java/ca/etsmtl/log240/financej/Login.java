f8c5fc646f680a419d4a7d51e6e6a2042e4ff42f
haimeurz <haimeurz@gmail.com>
2023-03-29 13:15:23 -0400

    enlever partie login pour les tests

--- src/main/java/ca/etsmtl/log240/financej/Login.java
+++ src/main/java/ca/etsmtl/log240/financej/Login.java
@@ -26,13 +26,13 @@ public class Login extends javax.swing.JFrame {
         // Create and display the form
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+//                new Login().setVisible(true);
             }
         });
     }
 
     public Login() {
-        initComponents();
+//        initComponents();
     }
 
     private void initComponents() {
