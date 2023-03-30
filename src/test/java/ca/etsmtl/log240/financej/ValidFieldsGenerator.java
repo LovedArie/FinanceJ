61f58d850064fd35533652cf4ef731c0596af2af
Fabien Valero <fabien.valero.1@ens.etsmtl.ca>
2023-03-30 15:10:56 -0400

    update tests

--- src/test/java/ca/etsmtl/log240/financej/ValidFieldsGenerator.java
+++ src/test/java/ca/etsmtl/log240/financej/ValidFieldsGenerator.java
@@ -123,4 +123,9 @@ public class ValidFieldsGenerator {
         }
         return payee;
     }
+
+    // generate random boolean
+    public static boolean generateRandomRec() {
+        return ThreadLocalRandom.current().nextBoolean();
+    }
 }
